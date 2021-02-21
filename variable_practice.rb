070a4b3783c0992433df68de7a359a14e5fdd771
Zach.Trokey <ztrokey@gmail.com>
2021-02-21 10:48:23 -0700

    Variable reassignments

--- variable_practice.rb
+++ variable_practice.rb
@@ -1,6 +1,10 @@
-name = "Chess"
-rook_limit = 8
-king_limit = 1
-pawn_backwards = false
-queen_diagonal = true
-pieces = ["Pawn", "Rook", "Knight", "Bishop", "Queen", "King"]
+word = "This is a string"
+num_str = "1, 2, 3, 4"
+int = 8
+flt = 3.14
+num_arr = [10, 11, 12, 13, 14]
+red = true
+
+word = "Changed string"
+num_str = "5, 6, 7, 8"
+int = 9
